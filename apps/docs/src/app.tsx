fcd374729c0d5108b89dbf08ee9fea4a861376b0
Pavaresorn Malai <61514399+Varkaria@users.noreply.github.com>
2023-07-01 12:40:31 +0700

    feat(docs): update design

--- apps/docs/src/app.tsx
+++ apps/docs/src/app.tsx
@@ -5,8 +5,8 @@ function App() {
         <div className="space-y-4">
           <img src="/logo.svg" className="h-10" />
           <h1 className="text-2xl">
-            A simple <b>CRUD</b> (Create Read Update Delete) API with lightning
-            fast performance with bun.js and easy to use.
+            A simple CRUD (Create Read Update Delete) API with lightning fast
+            performance with bun.js and easy to use.
           </h1>
           <div className="flex items-center gap-2 text-black/60 text-sm">
             Created by{' '}
@@ -28,7 +28,51 @@ function App() {
         <div className="opacity-60 font-medium">Playground Here</div>
       </main>
       <article>
-        <div className="opacity-60 font-medium">How to use Here</div>
+        <div className="opacity-60 font-medium">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          fringilla leo sed justo blandit, in dictum nunc varius. Quisque
+          scelerisque, diam et dictum mattis, sem diam accumsan eros, in
+          accumsan magna elit in diam. Aliquam tempus ex nec mauris venenatis,
+          in egestas lectus tempus. Nam nec nibh ante. In gravida vestibulum ex
+          nec laoreet. Vestibulum eu nunc et nibh accumsan mattis. Pellentesque
+          non ex nec mi viverra viverra sit amet et tortor. Proin lobortis
+          finibus mauris, sit amet aliquam tortor tristique quis. Integer
+          vestibulum nisl eget magna malesuada, non lobortis ipsum fringilla.
+          Aenean molestie mattis elit, eget rhoncus diam pellentesque egestas.
+          Ut euismod, justo in faucibus fermentum, orci lorem varius orci,
+          ullamcorper finibus tortor risus vel dolor. Integer elementum lorem
+          lorem, id sagittis libero dapibus ac. Nam non urna odio. Ut congue
+          neque velit. Sed et orci non turpis egestas fringilla ut sed nibh.
+          Fusce lobortis ullamcorper nibh, eget congue libero tincidunt sit
+          amet. Ut gravida felis quis turpis tincidunt dapibus. Aenean vehicula
+          ultricies lorem ac hendrerit. Sed id commodo massa. Nulla ullamcorper
+          nulla eros, hendrerit rhoncus nibh molestie quis. Nullam malesuada mi
+          orci, nec porttitor nisl ultrices sit amet. Praesent euismod porttitor
+          ante et cursus. In malesuada ligula in ligula porta, ut sagittis diam
+          maximus. Nullam magna velit, auctor at nulla quis, bibendum dictum
+          turpis. Sed ac consectetur ligula, sit amet mollis tortor. Sed a
+          venenatis felis. Nullam at metus ligula. Donec interdum a felis non
+          placerat. Nunc iaculis mollis maximus. Sed sagittis varius commodo.
+          Quisque sed diam ut velit viverra tempus nec in massa. Nullam suscipit
+          non leo ultrices dignissim. Vestibulum tristique interdum lacinia.
+          Nulla facilisi. Sed quam diam, convallis at felis sit amet, vulputate
+          suscipit sapien. Etiam ultrices eget nisi dictum scelerisque. Duis
+          lorem est, suscipit non gravida sit amet, cursus in metus. Nullam
+          tempor tincidunt ex, vel dictum metus feugiat et. In convallis eros
+          augue, eu euismod leo egestas eu. Donec dapibus ante at orci congue
+          interdum. Morbi metus elit, gravida sed ornare sit amet, pretium ac
+          velit. Mauris volutpat interdum ipsum. Pellentesque quis lacus at nunc
+          euismod tempus vitae et metus. Mauris luctus ultrices lectus, eget
+          molestie enim fermentum eget. Quisque sit amet augue id nunc ornare
+          efficitur ut vel tellus. Maecenas justo libero, aliquam vitae augue a,
+          pulvinar aliquet nibh. Morbi tempor molestie laoreet. Nullam
+          scelerisque imperdiet nisl id tincidunt. Aenean eu congue magna.
+          Curabitur vel sollicitudin arcu. Donec tincidunt non eros vitae
+          congue. Sed dictum, felis eu interdum pharetra, turpis lectus molestie
+          lorem, ac mattis odio tellus in neque. Aenean blandit, odio fermentum
+          blandit commodo, felis quam aliquam ligula, ac pulvinar odio dolor
+          commodo urna.
+        </div>
       </article>
     </>
   )
